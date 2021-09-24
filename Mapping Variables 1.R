9b32c21de4acaf02ff933085a1553e58da57d0bc
Raghav V <75249033+beansmans@users.noreply.github.com>
2021-09-24 12:20:31 +0530

    Add files via upload

---
+++ Mapping Variables 1.R
@@ -0,0 +1,36 @@
+library(ncdf4)
+library(fields)
+nc <- nc_open(choose.files())
+nc
+
+long <- ncvar_get(nc, "longitude")
+lati <- ncvar_get(nc, "latitude")
+
+longs <- sort(long)
+latis <- sort(lati)
+
+tp <- ncvar_get(nc, "tp", start = c(1 ,1 , 1), count = c(-1 , -1, 1))
+e <- ncvar_get(nc, "e", start = c(1 ,1 , 1), count = c(-1 , -1, 1))
+skt <- ncvar_get(nc, "skt", start = c(1 ,1 , 1), count = c(-1 , -1, 1))
+lai_hv <- ncvar_get(nc, "lai_hv", start = c(1 ,1 , 1), count = c(-1 , -1, 1))
+lavi_lv <- ncvar_get(nc, "lai_lv", start = c(1 ,1 , 1), count = c(-1 , -1, 1))
+
+png("tp.png", width = 800, height = 1000)
+image.plot(longs, latis, tp)
+dev.off()
+
+png("e.png", width = 800, height = 1000)
+image.plot(longs, latis, e)
+dev.off()
+
+png("skt.png", width = 800, height = 1000)
+image.plot(longs, latis, skt)
+dev.off()
+
+png("lai_hv.png", width = 800, height = 1000)
+image.plot(longs, latis, lai_hv)
+dev.off()
+
+png("lai_lv.png", width = 800, height = 1000)
+image.plot(longs, latis, lavi_lv)
+dev.off()
