e0b6aff696de3f803af5bf50c46e5f19651f1da0
Raghav V <75249033+beansmans@users.noreply.github.com>
2021-09-24 12:23:57 +0530

    Update Mapping Variables 1.R

--- Mapping Variables 1.R
+++ Mapping Variables 1.R
@@ -1,20 +1,25 @@
 library(ncdf4)
 library(fields)
+#loading nc file
 nc <- nc_open(choose.files())
 nc
 
+#extracting longitudes and latitudes for mapping
 long <- ncvar_get(nc, "longitude")
 lati <- ncvar_get(nc, "latitude")
 
+#sorting longitudes and latitudes 
 longs <- sort(long)
 latis <- sort(lati)
 
+#sxtracting the 5 variables in the dataset on the bases of: longitude, latitude, time
 tp <- ncvar_get(nc, "tp", start = c(1 ,1 , 1), count = c(-1 , -1, 1))
 e <- ncvar_get(nc, "e", start = c(1 ,1 , 1), count = c(-1 , -1, 1))
 skt <- ncvar_get(nc, "skt", start = c(1 ,1 , 1), count = c(-1 , -1, 1))
 lai_hv <- ncvar_get(nc, "lai_hv", start = c(1 ,1 , 1), count = c(-1 , -1, 1))
 lavi_lv <- ncvar_get(nc, "lai_lv", start = c(1 ,1 , 1), count = c(-1 , -1, 1))
 
+#plotting the variables using image.plot from the fields package and saving them as .png files
 png("tp.png", width = 800, height = 1000)
 image.plot(longs, latis, tp)
 dev.off()
